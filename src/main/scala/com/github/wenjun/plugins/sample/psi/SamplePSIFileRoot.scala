3c563e5ccc293e0de71ee041f17375acbdec4ebe
WenjunHuang <55865950@qq.com>
2020-05-08 23:56:41 +0800

    update

--- src/main/scala/com/github/wenjun/plugins/sample/psi/SamplePSIFileRoot.scala
+++ src/main/scala/com/github/wenjun/plugins/sample/psi/SamplePSIFileRoot.scala
@@ -1,10 +1,16 @@
 package com.github.wenjun.plugins.sample.psi
 
-import com.github.wenjun.plugins.sample.SampleLanguage
+import com.github.wenjun.plugins.sample.{Icons, SampleFileType, SampleLanguage}
 import com.intellij.extapi.psi.PsiFileBase
-import com.intellij.psi.FileViewProvider
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.psi.{FileViewProvider, PsiElement, PsiNamedElement}
+import javax.swing.Icon
 import org.antlr.intellij.adaptor.psi.ScopeNode
 
 class SamplePSIFileRoot(viewProvider:FileViewProvider) extends PsiFileBase(viewProvider,SampleLanguage.INSTANCE) with ScopeNode {
+  override def resolve(element: PsiNamedElement): PsiElement = ???
 
+  override def getFileType: FileType = SampleFileType.INSTANCE
+
+  override def getIcon(flags: Int): Icon = Icons.SampleIcon
 }
---
+++ src/main/scala/com/github/wenjun/plugins/sample/psi/VardefSubtree.scala
@@ -0,0 +1,7 @@
+package com.github.wenjun.plugins.sample.psi
+
+import org.antlr.intellij.adaptor.psi.IdentifierDefSubtree
+
+class VardefSubtree extends IdentifierDefSubtree {
+
+}
